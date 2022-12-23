b0b2b1101f786d2f0b75c2b874fa8dcb6c07fcbd
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-12-23 23:15:26 +0800

    http add RequestDataWithHeaders

--- web/http.go
+++ web/http.go
@@ -124,6 +124,33 @@ func RequestDataWith(client *http.Client, url, method, referer, ua string) (data
 	return
 }
 
+// RequestDataWithHeaders 使用自定义请求头获取数据
+func RequestDataWithHeaders(client *http.Client, url, method string, setheaders func(*http.Request) error) (data []byte, err error) {
+	// 提交请求
+	var request *http.Request
+	request, err = http.NewRequest(method, url, nil)
+	if err == nil {
+		// 增加header选项
+		err = setheaders(request)
+		if err != nil {
+			return
+		}
+		var response *http.Response
+		response, err = client.Do(request)
+		if err != nil {
+			return
+		}
+		if response.StatusCode != http.StatusOK {
+			s := fmt.Sprintf("status code: %d", response.StatusCode)
+			err = errors.New(s)
+			return
+		}
+		data, err = io.ReadAll(response.Body)
+		response.Body.Close()
+	}
+	return
+}
+
 // GetData 获取数据
 func GetData(url string) (data []byte, err error) {
 	var response *http.Response
