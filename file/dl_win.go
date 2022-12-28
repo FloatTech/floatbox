a2bededce74d5eeb89413fef02c19e34f9bbb139
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-12-28 23:02:04 +0800

    update win32 dlto

--- file/dl_win.go
+++ file/dl_win.go
@@ -4,33 +4,57 @@
 package file
 
 import (
+	"strings"
 	"syscall"
 	"unsafe"
+
+	"github.com/pkg/errors"
 )
 
 var (
 	urlmon                   = syscall.MustLoadDLL("Urlmon.dll")
 	processURLDownloadToFile = urlmon.MustFindProc("URLDownloadToFileW")
+	processIsValidURL        = urlmon.MustFindProc("IsValidURL")
+)
+
+const (
+	CP_UTF8 = 65001
+)
+
+var (
+	k32                        = syscall.MustLoadDLL("Kernel32.dll")
+	processMultiByteToWideChar = k32.MustFindProc("MultiByteToWideChar")
+)
+
+var (
+	ErrInvalidURL = errors.New("invalid url")
 )
 
 func DownloadTo(url, file string) error {
-	u := []rune(url)
-	u16 := make([]uint16, len(u)+1)
-	for i, c := range u {
-		u16[i] = uint16(c)
+	file = strings.ReplaceAll(file, "/", "\\")
+	u16 := make([]uint16, len(url)*4)
+	ret, _, err := processMultiByteToWideChar.Call(CP_UTF8, 0, *(*uintptr)(unsafe.Pointer(&url)), uintptr(len(url)), *(*uintptr)(unsafe.Pointer(&u16)), uintptr(len(u16)))
+	if ret == 0 {
+		return errors.Wrap(err, "url")
 	}
-	f := []rune(file)
-	f16 := make([]uint16, len(f)+1)
-	for i, c := range f {
-		f16[i] = uint16(c)
+	u16 = u16[:ret]
+	ret, _, _ = processIsValidURL.Call(0, *(*uintptr)(unsafe.Pointer(&u16)), 0)
+	if ret != 0 {
+		return ErrInvalidURL
 	}
-	ret, _, err := processURLDownloadToFile.Call(
+	f16 := make([]uint16, len(file)*4)
+	ret, _, err = processMultiByteToWideChar.Call(CP_UTF8, 0, *(*uintptr)(unsafe.Pointer(&file)), uintptr(len(file)), *(*uintptr)(unsafe.Pointer(&f16)), uintptr(len(f16)))
+	if ret == 0 {
+		return errors.Wrap(err, "file")
+	}
+	f16 = f16[:ret]
+	ret, _, _ = processURLDownloadToFile.Call(
 		0,
 		*(*uintptr)(unsafe.Pointer(&u16)),
 		*(*uintptr)(unsafe.Pointer(&f16)),
 		0, 0)
 	if ret == 0 {
-		err = nil
+		return nil
 	}
-	return err
+	return syscall.Errno(ret)
 }
