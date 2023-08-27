f0865337a824f95a473b56be42609b384822aff8
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-08-28 00:04:15 +0800

    feat: thread-safe cached file/dl

--- file/dl.go
+++ file/dl.go
@@ -3,20 +3,66 @@ package file
 
 import (
 	"crypto/tls"
+	"errors"
 	"io"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/RomiChan/syncx"
 )
+
+type dlcache syncx.Map[string, error]
+
+func (dlc *dlcache) wait(url string) error {
+	if err, loaded := (*syncx.Map[string, error])(dlc).LoadOrStore(url, errDlStatusDoing); loaded {
+		if err != errDlStatusDoing {
+			return err
+		}
+		t := time.NewTicker(time.Second)
+		defer t.Stop()
+		i := 0
+		for range t.C {
+			if err, ok := (*syncx.Map[string, error])(dlc).Load(url); ok && err != errDlStatusDoing {
+				return err
+			}
+			i++
+			if i > 60 {
+				break
+			}
+		}
+		return errDlStatusTimeout
+	}
+	time.AfterFunc(time.Minute*2, func() {
+		(*syncx.Map[string, error])(dlc).Delete(url)
+	})
+	return errDlContinue
+}
+
+func (dlc *dlcache) set(url string, err error) {
+	(*syncx.Map[string, error])(dlc).Store(url, err)
+}
 
 var (
 	tr = &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	nochkcrtcli = &http.Client{Transport: tr}
+	dlmap       = dlcache{}
+)
+
+var (
+	errDlContinue      = errors.New("continue")
+	errDlStatusDoing   = errors.New("downloading")
+	errDlStatusTimeout = errors.New("download timeout")
 )
 
 // DownloadTo 下载到路径
 func DownloadTo(url, file string) error {
+	err := dlmap.wait(url)
+	if err != errDlContinue {
+		return err
+	}
 	resp, err := http.Get(url)
 	if err == nil {
 		var f *os.File
@@ -27,11 +73,16 @@ func DownloadTo(url, file string) error {
 		}
 		resp.Body.Close()
 	}
+	dlmap.set(url, err)
 	return err
 }
 
 // NoChkCrtDownloadTo 下载到路径
 func NoChkCrtDownloadTo(url, file string) error {
+	err := dlmap.wait(url)
+	if err != errDlContinue {
+		return err
+	}
 	resp, err := nochkcrtcli.Get(url)
 	if err == nil {
 		var f *os.File
@@ -42,5 +93,6 @@ func NoChkCrtDownloadTo(url, file string) error {
 		}
 		resp.Body.Close()
 	}
+	dlmap.set(url, err)
 	return err
 }
